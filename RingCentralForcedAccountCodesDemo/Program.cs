37171e66dc2fc4a817a5767e34b744f2794edab1
Tyler Long <tyler4long@gmail.com>
2020-10-27 17:34:16 -0700

    dotenv.net

--- RingCentralForcedAccountCodesDemo/Program.cs
+++ RingCentralForcedAccountCodesDemo/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Threading.Tasks;
+using dotenv.net;
+using RingCentral;
 
 namespace RingCentralForcedAccountCodesDemo
 {
@@ -6,7 +9,29 @@ namespace RingCentralForcedAccountCodesDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DotEnv.Config(true);
+
+            Task.Run(async () =>
+            {
+                var rc = new RestClient(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+                );
+
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                var eventFilters = new[]
+                {
+                    "/restapi/v1.0/account/~/extension/~/telephony/sessions"
+                };
+                Console.WriteLine(rc.token.access_token);
+                // await Task.Delay(999999999);
+                await rc.Revoke();
+            }).GetAwaiter().GetResult();
         }
     }
 }
